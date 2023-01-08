f532a9e0ddc26579bacc429a16b27ad280e81e75
Beletskyi-Moawad Martin <m.beleckii@alef.im>
2023-01-08 21:02:14 +0300

    Добавлен тип для каналов

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,5 +1,10 @@
 import { atom } from 'nanostores';
 
+interface RSSChannel {
+    name: string,
+    url: string
+};
+
 export const modalStore = atom(false);
 export const urlStore = atom('');
-export const rssStore = atom([]);
+export const rssStore = atom([] as Array<RSSChannel>);
